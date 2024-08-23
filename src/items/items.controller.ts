81f2451b620c5c6d555aa3952da6308560d2a36f
UgurCanODAK <ugurodak@gmail.com>
2024-08-23 22:44:00 +0300

    send object

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,17 +1,23 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, Req, Res, Param } from '@nestjs/common';
 import {CreateItemDto} from "./dto/create-item.dto";
-
+import {Response, Request} from 'express';
 
 @Controller('items')
 export class ItemsController {
 
     @Get()
-    findAll():string {
-        return 'Get all items';
+    findAll(@Req() req: Request, @Res() res: Response): Response {
+        console.log(req.url)
+        return res.send('Hello World');
     };
+
+    @Get(':id')
+    findOne(@Param() param):string{
+        return `Item ${param.id}`
+    }
 
     @Post()
     create(@Body() createItemDto: CreateItemDto):string {
         return `Name :${createItemDto.name} Desc: ${createItemDto.description}`
     }
-};
+}
